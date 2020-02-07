209d19640c0ead3e868e104eff9fd81a32e03ef9
PaulDIG <paul@digraci.net>
2020-02-07 16:04:45 +0100

    Modif de 'Serveur.py'.

--- Serveur.py
+++ Serveur.py
@@ -6,20 +6,16 @@ host='0.0.0.0'
 port=8000
 etat=True
 
-def Lecture_Client(client,iport):
+def Lecture_Client(client,adresse):
     
-    ip_client=iport[0]
-    port_client=iport[1]
-    
-    print(":: Connexion rentrante :: \nClient : " .format(client))
-    print("\nIP : " .format(ip_client))
+    print("\n:: Connexion rentrante :: \nIP : {}" .format(adresse))
     
     msg_recu = client.recv(1024)
     msg_recu = msg_recu.decode()
     
-    print("Reçu {}".format(msg_recu))
+    print("Reçu : {}".format(msg_recu))
     
-    client.send("5 / 5")
+    client.send(b"5 / 5")
     
     if msg_recu=="STOP":
         etat=False
@@ -39,7 +35,7 @@ while etat:
     
     try:
         Client, Adresse = serveur.accept()
-        threading._start_new_thread(Lecture_Client(Client,Adresse))
+        Lecture_Client(Client,Adresse)
         
     except:
         pass
